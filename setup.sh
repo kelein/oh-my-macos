fb2937ce0f32a7e0fd441c83fdabfb08abb15449
rainplus <rainplus@foxmail.com>
2018-03-20 22:39:50 +0800

    修改获取方式

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # 使用安装sh的脚本
 
-cd $HOME/.m2 && curl -O https://raw.githubusercontent.com/rainplus/oh-my-macos/master/sh/mvnmirror.sh
+cd $HOME/.m2 && curl https://raw.githubusercontent.com/rainplus/oh-my-macos/master/sh/mvnmirror.sh >\
+$HOME/.m2/mvnmirror.sh
 
 
 ln -fs $HOME/.m2/mvnmirror.sh /usr/local/bin/mvnmirror
 
-cd $HOME/.ssh && curl -O https://raw.githubusercontent.com/rainplus/oh-my-macos/master/sh/expext2shell.sh
+cd $HOME/.ssh && wget
+https://raw.githubusercontent.com/rainplus/oh-my-macos/master/sh/expect2shell.sh \
+>$HOME/.ssh/expect2shell.sh
 
-curl https://raw.githubusercontent.com/rainplus/oh-my-macos/master/sh/jump2shell.sh
+wget -O https://raw.githubusercontent.com/rainplus/oh-my-macos/master/sh/jump2shell.sh \
+	$HOME/.ssh/jump2shell.sh
 
 chmod 777 $HOME/.m2/mvnmirror.sh $HOME/.ssh/expect2shell.sh $HOME/.ssh/jump2shell.sh
 
